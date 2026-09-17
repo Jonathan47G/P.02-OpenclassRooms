ab2622ab8bdb14f1e86e4abcecac11534c9396df
Jonathan47G <guestin.jonathan@gmail.com>
2026-09-17 12:46:51 +0200

    fix: clear comment/variable inutile

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,3 +1,4 @@
+
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
@@ -5,35 +6,31 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+
+	private static int headacheCount = 0;
+	private static int rashCount = 0;
+	private static int pupilCount = 0;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
 		while (line != null) {
-			i++;	// increment i
 			System.out.println("symptom from file: " + line);
+
 			if (line.equals("headache")) {
 				headacheCount++;
 				System.out.println("number of headaches: " + headacheCount);
-			}
-			else if (line.equals("rush")) {
+			} else if (line.equals("rush")) {
 				rashCount++;
-			}
-			else if (line.contains("pupils")) {
+			} else if (line.contains("pupils")) {
 				pupilCount++;
 			}
 
-			line = reader.readLine();	// get another symptom
+			line = reader.readLine();
 		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
+
+		FileWriter writer = new FileWriter("result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
