369ddbcdbcc0638e1aae8463814e0f3dfe4346f4
Jonathan47G <guestin.jonathan@gmail.com>
2026-09-18 13:40:27 +0200

    fix: compte tout les symptoms et mise en place d'un try catch.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,36 +4,45 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
 
 public class AnalyticsCounter {
 
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
+	public static void main(String[] args) {
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+		Map<String, Integer> symptomCounts = new TreeMap<>();
 
-		while (line != null) {
-			System.out.println("symptom from file: " + line);
+		try (BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"))) {
 
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
-			} else if (line.equals("rash")) {
-				rashCount++;
-			} else if (line.contains("pupils")) {
-				pupilCount++;
+			String line = reader.readLine();
+
+			while (line != null) {
+				symptomCounts.put(line, symptomCounts.getOrDefault(line, 0) + 1
+				);
+
+				line = reader.readLine();
 			}
 
-			line = reader.readLine();
+		} catch (IOException e) {
+			System.err.println("Error writing results: " + e.getMessage());
+			return;
 		}
 
-		FileWriter writer = new FileWriter("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+
+        try(FileWriter writer = new FileWriter("result.out")) {
+
+		for (Map.Entry<String, Integer> entry : symptomCounts.entrySet()) {
+			writer.write(
+					entry.getKey() + ": " + entry.getValue() + "\n"
+			);
+		}
+	} catch (IOException e) {
+			System.err.println("Error writing results: " + e.getMessage());
+	}
+
+
 	}
 }
